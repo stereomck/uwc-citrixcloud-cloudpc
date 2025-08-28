5e12ea676ea15dce15fe4825fdb7446596dcc705
Michael Kent <m.kent@loginvsi.com>
2025-08-28 16:28:19 -0700

    empty base csharp script for post resource launch

---
+++ src/SessionWindowHandler.cs
@@ -0,0 +1,12 @@
+// TARGET:msedgewebview2.exe
+// START_IN:
+
+using LoginPI.Engine.ScriptBase;
+using LoginPI.Engine.ScriptBase.Components;
+public class SessionWindowHandler : ScriptBase
+{
+
+    void Execute()
+    {
+    }
+}
